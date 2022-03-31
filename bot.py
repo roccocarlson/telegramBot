2270bab07a017baf135832a15affa96d534618ad
Rocco Carlson <roccoc@mtu.edu>
2022-03-31 06:41:24 -0400

    Fixed issue #1

--- bot.py
+++ bot.py
@@ -9,18 +9,18 @@ from telegram import Update
 from urllib import request
 import subprocess, logging
 
+global HOSTNAME
+HOSTNAME = '0.0.0.0'
+
 # CHAT ID FOR SPECIFIC CHAT - BOT WILL NOT WORK IN ANY OTHER CHAT
-with open('chatid.txt', 'r') as f:
+with open('/home/pi/Documents/telegramBot/chatid.txt', 'r') as f:
     CHAT_ID = f.readline()
     print("Chat ID: "+CHAT_ID)
 
 # API ID FOR BOT - OBTAINED FROM BOT FATHER
-with open('apiid.txt', 'r') as f:
+with open('/home/pi/Documents/telegramBot/apiid.txt', 'r') as f:
     API_ID = f.readline()
     print("API ID: "+API_ID)
-
-# HOSTNAME FOR THE DEVICE - TO BE SENT ON /ip REQUEST
-HOSTNAME = subprocess.getoutput('hostname -I').split(" ", 2)[0]
 
 def wait_for_internet_connection():
     while True:
@@ -35,6 +35,7 @@ def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=CHAT_ID, text='Hello there')
 
 def ip(update: Update, context: CallbackContext):
+    HOSTNAME = subprocess.getoutput('hostname -I').split(" ", 2)[0]
     print('Sending IP...', end='')
     context.bot.send_message(chat_id=CHAT_ID, text=('IP: ' + HOSTNAME))
     print(' Sent!')
@@ -43,6 +44,9 @@ def main():
 
     # WAIT UNTIL CONNECTED TO INTERNET - IMPORTANT FOR SYSTEMD
     wait_for_internet_connection()
+
+    # HOSTNAME FOR THE DEVICE - TO BE SENT ON /ip REQUEST
+    HOSTNAME = subprocess.getoutput('hostname -I').split(" ", 2)[0]
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     
